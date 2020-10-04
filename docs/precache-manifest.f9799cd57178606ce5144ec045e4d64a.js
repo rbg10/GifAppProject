5124e7e0be81dfc0a3c475a84bb52d0757d732eb
renebravo10@gmail.com <rene.bravo@mbww.com>
2020-10-04 16:16:19 -0500

    docs agregados

---
+++ docs/precache-manifest.f9799cd57178606ce5144ec045e4d64a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fa6b3b549b52663be12d1b3bbe10c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26ee8f0334887635ba33",
+    "url": "/static/css/main.5aec22d1.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ee8f0334887635ba33",
+    "url": "/static/js/main.3911c8e4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
